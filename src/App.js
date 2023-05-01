c61aa992b19eee6d1cee77ce139631bd372558b0
Matias Zanan <matiaszanan@gmail.com>
2023-05-01 15:34:50 -0300

    [API] Fix openOrder()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
-import { openOrder } from "./modules/API";
+import { openOrder, cancelAll, listBTCUSDT } from "./modules/API";
 
 const App = () => {
   return (
     <div>
-      <h2>Welcome to React</h2>
+      <h2>GVT CopyTrading</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "200px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100px",
+            height: "200px",
+            justifyContent: "space-between",
+          }}
+        >
+          <button onClick={() => listBTCUSDT()}>BTCUSDT Open Orders</button>
 
-      <button onClick={() => openOrder()}>Long BTCUSDT</button>
+          <button
+            onClick={() => openOrder("Buy")}
+            style={{ backgroundColor: "green" }}
+          >
+            Long BTCUSDT
+          </button>
+
+          <button
+            onClick={() => openOrder()}
+            style={{ backgroundColor: "red" }}
+          >
+            Short BTCUSDT
+          </button>
+
+          <button onClick={() => cancelAll()}>Cancel All Orders</button>
+        </div>
+      </div>
     </div>
   );
 };
