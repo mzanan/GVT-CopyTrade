8c45f253c987a6bdebcdd8de8c0f49d39b87ae7c
Matias Zanan <matiaszanan@gmail.com>
2023-05-29 15:24:11 -0300

    add openOrder validation

--- src/modules/Orders/executeOrder.js
+++ src/modules/Orders/executeOrder.js
@@ -6,24 +6,42 @@ export const executeOrder = async (side, symbol) => {
   const createOrder = await openOrder(side, symbol);
   console.log(createOrder);
 
+  if (Object.keys(createOrder.data).length === 0) {
+    return {
+      createOrder: {
+        status: createOrder.status,
+      },
+    };
+  } else if (createOrder.data) {
+    return {
+      createOrder: {
+        description: createOrder.description,
+        status: createOrder.status,
+      },
+    };
+  }
+
   const listOrder = await getOrder(symbol);
   console.log(listOrder);
+
+  if (listOrder) {
+    return {
+      listOrder: {
+        description: listOrder.description,
+        status: listOrder.status,
+      },
+    };
+  }
 
   const slTpResponse = await setStopProfit(createOrder, listOrder);
   console.log(slTpResponse);
 
-  return {
-    createOrder: {
-      description: createOrder.description,
-      status: createOrder.status,
-    },
-    listOrder: {
-      description: listOrder.description,
-      status: listOrder.status,
-    },
-    slTpResponse: {
-      description: slTpResponse.description,
-      status: slTpResponse.status,
-    },
-  };
+  if (slTpResponse) {
+    return {
+      slTpResponse: {
+        description: slTpResponse.description,
+        status: slTpResponse.status,
+      },
+    };
+  }
 };
